bad07740ff6d5de68d34c536e53b11ebaa8a7015
parham monfared <parhammonfared@parhams-Mac.local>
2018-03-18 06:06:36 -0700

    Make connection to tableViewController/ Add identifier to table cell / Add navigation Bar

--- DoList/ToDoListTableViewController.swift
+++ DoList/ToDoListTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoListTableViewController: UITableViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
